60c57adbcd8bb5a9ab72d981f9e48e653b88f848
ramnathgknaivaidya <ramnathgk.naivaidya@gmail.com>
2026-07-24 13:17:30 +0530

    Update works.tsx

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,10 +1,12 @@
 import PortfolioSection from "@/sections/PortfolioSection";
+import IntegrationsSection from "@/sections/IntegrationsSection";
 import Footer from "@/sections/Footer";
 
 export default function Works() {
   return (
     <>
       <PortfolioSection id="portfolio" />
+      <IntegrationsSection />
       <Footer />
     </>
   );
